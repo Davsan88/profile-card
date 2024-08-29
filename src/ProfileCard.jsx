1bb814106195e745bb43f7b7e78110377075653b
DavSan <david.sanchez.martinez@outlook.com>
2024-08-29 14:31:35 +0100

    Update ProfileCard.jsx

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -5,7 +5,7 @@ function ProfileCard({ image, name, jobTitle, bio }) {
         <div className="profile-card">
             <img src={image} alt={`${name}'s profile`} className="profile-image" />
             <h2 className="profile-name">{name}</h2>
-            <h4>{jobTitle}</h4>
+            <h4 className="profile-job">{jobTitle}</h4>
             <p>{bio}</p>
         </div>
     );
